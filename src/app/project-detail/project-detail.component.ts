0d86d20c9c8062f481f45dbde8b662cd7618ff5f
keei <jozef.vanicky@gmail.com>
2018-12-11 15:47:06 +0100

    Tooltips added

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProjectService} from '../_service/project.service';
 
 @Component({
   selector: 'app-project-detail',
@@ -9,6 +10,7 @@ export class ProjectDetailComponent implements OnInit {
   positions: Position[];
 
   constructor(
+      private projectService: ProjectService
 
   ) { }
 
