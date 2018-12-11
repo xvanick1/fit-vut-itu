461456ab22a7aa04da14586a9eea8dc097342f5a
keei <jozef.vanicky@gmail.com>
2018-12-11 13:08:49 +0100

    ProjectDetail edits

---
+++ src/app/project-detail/project-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-detail',
+  templateUrl: './project-detail.component.html',
+  styleUrls: ['./project-detail.component.scss']
+})
+export class ProjectDetailComponent implements OnInit {
+  positions: Position[];
+
+  constructor(
+
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+}
